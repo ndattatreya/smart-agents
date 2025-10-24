5d8b829ba9f64714aa5b072db4d91eda3a0a696f
Dattatreya <dattatreya.nammina1@gmail.com>
2025-10-24 21:16:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,10 @@ connectDB();
 // ========================
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://smart-agents.vercel.app"
+    ],
     credentials: true,
   })
 );
